07752fba612d983cc12eea55cc1a8a5b00e372b2
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-20 23:53:49 +0530

    add backend/controllers/itemController.js

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,24 @@
+const Task = require('../models/Item');
+
+exports.getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
+
+exports.createTask = async (req, res) => {
+  const newTask = new Task(req.body);
+  await newTask.save();
+  res.status(201).json(newTask);
+};
+
+exports.deleteTask = async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.status(204).send();
+};
+
+exports.toggleTask = async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  task.completed = !task.completed;
+  await task.save();
+  res.json(task);
+};
